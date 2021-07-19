c6ad753b5bf8fad36f16c49b8c7a9c09fe8d2877
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-19 13:25:52 -0400

    Set up data for fetching and displaying projects

---
+++ assets/js/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        title: "Move Towards the Movies",
+        repo: "move-towards-the-movies",
+        screenshot: "../images/screenshots/move-towards-the-movies.png",
+        collaborators: [
+            "Chrisbr916",
+            "Bluekev22",
+            "eamahma"
+        ]
+    },
+    {
+        title: "Weather Dashboard",
+        repo: "weather-dashboard",
+    },
+    {
+        title: "Work Day Planner",
+        repo: "work-day-planner"
+    },
+    {
+        title: "JavaScript Quiz",
+        repo: "javascript-quiz"
+    },
+    {
+        title: "Password Generator",
+        repo: "password-generator"
+    }
+]
