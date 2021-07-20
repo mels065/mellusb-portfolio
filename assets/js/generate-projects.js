887a4ca074ac60ef5737aa5e6575d4398efa915f
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-19 21:31:35 -0400

    Style projects with Bulma

--- assets/js/generate-projects.js
+++ assets/js/generate-projects.js
@@ -1,15 +1,19 @@
-projects.forEach(({ repo, title, screenshot, multipleScreenshots }) => {
+projects.forEach(({ repo, title, screenshot, multipleScreenshots } , i) => {
     const screenshotPath = screenshot
         ? screenshot 
         : `https://raw.githubusercontent.com/mels065/${repo}/main/assets/images/screenshots/screenshot${multipleScreenshots ? 1 : ""}.png`;
     const el = $(
         `
-        <figure class="project">
-            <a href="https://mels065.github.io/${repo}">
-                <h3>${title}</h3>
+            <figure class="project card column ${i === 0 ? "is-full" : "is-one-third-desktop is-half-tablet is-full-mobile"}">
+                <header class="has-text-centered">
+                    <h3 class="title">${title}</h3>
+                    <ul class="project-links columns">
+                        <li class="project-link project-demo column"><a class="button is-black" target="_blank" href="https://mels065.github.io/${repo}">Demo</a></li>
+                        <li class="project-link project-repo column"><a class="button is-black" target="_blank" href="https://www.github.com/mels065/${repo}">Repo</a></li>
+                    </ul>
+                </header>
                 <img src="${screenshotPath}" alt="${title}" />
-            </a>
-        </figure>
+            </figure>
         `
     );
     $("#projects").append(el);
