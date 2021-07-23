29b4cd144ab906b11b462ec1e960484f9eca22de
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-22 22:44:41 -0400

    Style the project cards some more

--- assets/js/generate-projects.js
+++ assets/js/generate-projects.js
@@ -11,15 +11,20 @@ projects.forEach(({
     const el = $(
         `
             <figure class="project card column ${i === 0 ? "is-full" : "is-one-third-desktop is-half-tablet is-full-mobile"}">
-                <header class="has-text-centered">
-                    <h3 class="title is-underlined">${title}</h3>
-                    <p class="subtitle">Collaborators: ${collaborators ? generateCollaborators(collaborators) : "None"}</p>
-                    <ul class="project-links columns">
-                        <li class="project-link project-demo column"><a class="button is-link" target="_blank" href="https://mels065.github.io/${repo}">Demo</a></li>
-                        <li class="project-link project-repo column"><a class="button is-link" target="_blank" href="https://www.github.com/mels065/${repo}">Repo</a></li>
-                    </ul>
+                <header class="card-header">
+                    <h3 class="title">${title}</h3>
                 </header>
-                <img src="${screenshotPath}" alt="${title}" />
+                <div class="card-content">
+                    <p class="card-text">Collaborators: ${collaborators ? generateCollaborators(collaborators) : "None"}</p>
+                    <ul class="project-links is-flex">
+                        <li class="project-link project-demo"><a class="button is-link is-outlined" target="_blank" href="https://mels065.github.io/${repo}">Demo</a></li>
+                        <li class="project-link project-repo"><a class="button is-link is-outlined" target="_blank" href="https://www.github.com/mels065/${repo}">Repo</a></li>
+                    </ul>
+                </div>
+                <hr />
+                <div class="card-image">
+                    <img class="image" src="${screenshotPath}" alt="${title}" />
+                </div>
             </figure>
         `
     );
