696ced72ea246b906cbd21af49058ecef26e03ee
Brandon Mellus <mellusbrandon@gmail.com>
2021-09-18 12:06:56 -0700

    Change node environment to staging for seeding data to deployed database

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else if (process.env.NODE_ENV === 'production') {
+} else if (process.env.NODE_ENV === 'staging') {
     sequelize = new Sequelize(
         process.env.PROD_DB_NAME,
         process.env.PROD_DB_USER,
